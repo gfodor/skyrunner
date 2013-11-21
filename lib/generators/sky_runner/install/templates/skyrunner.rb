abcf7a2446f9d697ac5ed7f3fabb34b7466285eb
Greg Fodor <gfodor@gmail.com>
2013-11-21 13:10:07 -0800

    update comments

--- lib/generators/sky_runner/install/templates/skyrunner.rb
+++ lib/generators/sky_runner/install/templates/skyrunner.rb
@@ -5,8 +5,10 @@ SkyRunner.setup do |config|
   config.dynamo_db_table_name = "skyrunner_jobs_#{Rails.env}"
   config.sqs_queue_name = "skyrunner_tasks_#{Rails.env}"
 
-  # Set the visibility timeout of queue items. If the consumer batch size (above) is set to 10,
-  # this should provide sufficient time for a consumer to process 10 tasks, for example. (default 90)
+  # Set the visibility timeout of queue items. 
+  #
+  # This should provide sufficient time for a consumer to process 10 tasks, since 10 items 
+  # are dequeued at a time from SQS.
   #
   # config.visibility_timeout = 90
 
